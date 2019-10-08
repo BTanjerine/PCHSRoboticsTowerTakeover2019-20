096e9e91ecc6be5a39345b96ff3efae210024d80
BT <2020brta0052@pchigh.k12.hi.us>
2019-10-07 15:37:11 -1000

    made a mistake moved to .h

--- include/robot-config.h
+++ include/robot-config.h
@@ -1,3 +1,12 @@
+#include <cmath>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+#include "v5.h"
+#include "v5_vcs.h"
+
 using namespace vex;
 
 extern brain Brain;
--- include/vex.h
+++ include/vex.h
@@ -7,14 +7,6 @@
 /*                                                                            */
 /*----------------------------------------------------------------------------*/
 //
-#include <cmath>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <math.h>
-
-#include "v5.h"
-#include "v5_vcs.h"
 #include "robot-config.h"
 #include "utilities.h"
 
