4ddc34012ed79df1beb029e622bac945b94579d0
BT <2020brta0052@pchigh.k12.hi.us>
2019-10-07 15:35:59 -1000

    move includes to robotConfig

--- src/robot-config.cpp
+++ src/robot-config.cpp
@@ -1,4 +1,11 @@
+#include <cmath>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
 
+#include "v5.h"
+#include "v5_vcs.h"
 
 using namespace vex;
 using signature = vision::signature;
